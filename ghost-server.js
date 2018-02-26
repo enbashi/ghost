de7e1b8ad11aece483e5ad00eb63bf1e234f006c
Rami Enbashi <oopson@gmail.com>
2018-02-26 09:39:42 -0500

    Updating ghost-server.js

--- ghost-server.js
+++ ghost-server.js
@@ -5,33 +5,26 @@ import Webtask from 'webtask-tools';
 import { MongoClient } from 'mongodb';
 import { ObjectID } from 'mongodb';
 
-const collection = 'my-collection';
+const collection = 'ghost-sessions';
 const server = express();
 
-
 server.use(bodyParser.json());
-server.get('/:_id', (req, res, next) => {
+
+
+
+server.post('/', (req, res, next) => {
   const { MONGO_URL } = req.webtaskContext.secrets;
+  const { _id } = req.body ;
+  // Do data sanitation here.
+  const model = req.body;
+  console.log('model', model);
   MongoClient.connect(MONGO_URL, (err, db) => {
-    const { _id } = req.params ;
     if (err) return next(err);
-    db.collection(collection).findOne({ _id: new ObjectID(_id) }, (err, result) => {
+    // db.collection(collection).insertOne(model, (err, result) => {
+    db.collection(collection).update({_id}, model, { upsert : true }, (err, result) => {
       db.close();
       if (err) return next(err);
       res.status(200).send(result);
-    });
-  });
-});
-server.post('/', (req, res, next) => {
-  const { MONGO_URL } = req.webtaskContext.secrets;
-  // Do data sanitation here.
-  const model = req.body;
-  MongoClient.connect(MONGO_URL, (err, db) => {
-    if (err) return next(err);
-    db.collection(collection).insertOne(model, (err, result) => {
-      db.close();
-      if (err) return next(err);
-      res.status(201).send(result);
     });
   });
 });
