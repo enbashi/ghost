2842a4757ccf251ee6e186f7689d02471621bb24
Rami Enbashi <oopson@gmail.com>
2018-02-26 11:47:41 -0500

    Update ghost-server.js

--- ghost-server.js
+++ ghost-server.js
@@ -9,8 +9,21 @@ const collection = 'ghost-sessions';
 const server = express();
 
 server.use(bodyParser.json());
-// server.use(bodyParser.json({limit: '5mb', extended: true}));
+// server.use(bodyParser.json({limit: '50mb'}));
 
+server.get('/:_id', (req, res, next) => {
+  const { _id } = req.params ;
+  const { MONGO_URL } = req.webtaskContext.secrets;
+
+  MongoClient.connect(MONGO_URL, (err, db) => {
+    if (err) return next(err);
+     db.collection(collection).findOne({ _id }, (err, result) => {
+       db.close();
+       if (err) return next(err);
+       res.status(200).send(result);
+     });
+  });
+});
 
 
 server.post('/:_id', (req, res, next) => {
